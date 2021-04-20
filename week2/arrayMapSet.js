eb51700ee11cb21c2e1dfe141b13aea78e588098
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-04-20 12:36:42 -0400

    fsw113Wk2

--- week2/arrayMapSet.js
+++ week2/arrayMapSet.js
@@ -251,8 +251,7 @@ CLO17: JavaScript:
 
 let students =[ {name:'John Masson'}, {name:'Don Vasco'}, {name:'Mary Landley'}
 let people = new Set(students)
+//Question: What is the value shown up in the console for the following statements
 
-
-
- console.log(people.has({name:'Dave Vasco'})
+console.log(people.has({name:'Dave Vasco'})
 
